af03aa3edd384a38b6783266affe5c4768ad1fac
Estebes <estebesm95@gmail.com>
2023-01-28 19:23:38 +0600

    bugfix/fixed_exports_error

--- src/store/api/authApi.ts
+++ src/store/api/authApi.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { LoginInput } from '../../pages/signin';
-import { RegisterInput } from '../../pages/signup';
+//import { LoginInput } from '../../pages/signin';
+//import { RegisterInput } from '../../pages/signup';
 import { IGenericResponse } from './types';
 import { userApi } from './userApi';
 
@@ -12,59 +12,59 @@ export const authApi = createApi({
         baseUrl: `${BASE_URL}/api/auth/`,
     }),
     endpoints: (builder) => ({
-        registerUser: builder.mutation<IGenericResponse, RegisterInput>({
-            query(data) {
-                return {
-                    url: 'register',
-                    method: 'POST',
-                    body: data,
-                };
-            },
+        // registerUser: builder.mutation<IGenericResponse, RegisterInput>({
+        //     query(data) {
+        //         return {
+        //             url: 'register',
+        //             method: 'POST',
+        //             body: data,
+        //         };
+        //     },
+        // }),
+        // loginUser: builder.mutation<
+        //     { access_token: string; status: string },
+        //     LoginInput
+        //     >({
+        //     query(data) {
+        //         return {
+        //             url: 'login',
+        //             method: 'POST',
+        //             body: data,
+        //             credentials: 'include',
+        //         };
+        //     },
+        //     async onQueryStarted(args, { dispatch, queryFulfilled }) {
+        //         try {
+        //             await queryFulfilled;
+        //             await dispatch(userApi.endpoints.getMe.initiate(null));
+        //         } catch (error) {}
+        //     },
         }),
-        loginUser: builder.mutation<
-            { access_token: string; status: string },
-            LoginInput
-            >({
-            query(data) {
-                return {
-                    url: 'login',
-                    method: 'POST',
-                    body: data,
-                    credentials: 'include',
-                };
-            },
-            async onQueryStarted(args, { dispatch, queryFulfilled }) {
-                try {
-                    await queryFulfilled;
-                    await dispatch(userApi.endpoints.getMe.initiate(null));
-                } catch (error) {}
-            },
-        }),
-        verifyEmail: builder.mutation<
-            IGenericResponse,
-            { verificationCode: string }
-            >({
-            query({ verificationCode }) {
-                return {
-                    url: `verifyemail/${verificationCode}`,
-                    method: 'GET',
-                };
-            },
-        }),
-        logoutUser: builder.mutation<void, void>({
-            query() {
-                return {
-                    url: 'logout',
-                    credentials: 'include',
-                };
-            },
-        }),
-    }),
+        // verifyEmail: builder.mutation<
+        //     IGenericResponse,
+        //     { verificationCode: string }
+        //     >({
+        //     query({ verificationCode }) {
+        //         return {
+        //             url: `verifyemail/${verificationCode}`,
+        //             method: 'GET',
+        //         };
+        //     },
+        // }),
+        // logoutUser: builder.mutation<void, void>({
+        //     query() {
+        //         return {
+        //             url: 'logout',
+        //             credentials: 'include',
+        //         };
+        //     },
+        // }),
+    //}),
 });
 
 export const {
-    useLoginUserMutation,
-    useRegisterUserMutation,
-    useLogoutUserMutation,
-    useVerifyEmailMutation,
+    // useLoginUserMutation,
+    // useRegisterUserMutation,
+    // useLogoutUserMutation,
+    // useVerifyEmailMutation,
 } = authApi;
